1207ed97ccf3928db328fe729ab68c0890a1d4ac
Suraz95 <shaik.suraz50@gmail.com>
2024-07-07 21:39:28 +0530

    v-1

---
+++ src/data/WorkData.js
@@ -0,0 +1,45 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Ecommerce Website",
+        description:"It is a MERN Stack Online Book Store - The Book Vault , It has an admin page to view about the customers ",
+        tags:["MongoDB","Express.js","React.js","Node.js"],
+        
+        demo:"https://the-book-vault-six.vercel.app/",
+        github:"https://github.com/Suraz95/the_book_vault.git"
+    },
+    {
+        id:2,
+        name:"Resume Builder",
+        description:"A Resume Builder website build in ReactJS. With editing and downloading",
+        tags:["React"],
+        
+        demo:"https://suraz-resume-builder-react.vercel.app",
+        github:"https://github.com/Suraz95/Suraz_resume_builder_react.git"
+        
+    },
+    {
+        id:3,
+        name:"Ecommerce Website",
+        description:"An ecommerce website made using React nad Redux-tool-kit with add to cart and wishlist functionality",
+        tags:["React","Redux-tool-kit"],
+        
+        demo:"https://suraz-ecommmerce-reduxtoolkit2.vercel.app/",
+        github:"https://github.com/Suraz95/ecommmerce-reduxtoolkit2.git"
+    },
+    {
+        id:4,
+        name:"Expense Tracker",
+        description:"An Expense Traker website which uses your local storage as your database and keeps track of your expenses ",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://suraz-expense-tracker.vercel.app/",
+        github:"https://github.com/Suraz95/expense-tracker.git"
+       
+    },
+   
+    
+  
+] 
