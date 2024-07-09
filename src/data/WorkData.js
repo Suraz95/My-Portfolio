3e07a734447ed1dcd27b4debc0d0bc54338e2c17
Suraz95 <shaik.suraz50@gmail.com>
2024-07-09 09:45:49 +0530

    added project pics

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,12 +1,15 @@
 // work data
-
+import bookvault from "../assets/ProjectImages/Thebookvault.png"
+import ecommerce from "../assets/ProjectImages/ecommerce.png"
+import resume from "../assets/ProjectImages/resumebuilder.png"
+import expense from "../assets/ProjectImages/expensetracker.png"
 export const Work = [
     {
         id:1,
         name:"Ecommerce Website",
         description:"It is a MERN Stack Online Book Store - The Book Vault , It has an admin page to view about the customers ",
         tags:["MongoDB","Express.js","React.js","Node.js"],
-        
+        image:bookvault,
         demo:"https://the-book-vault-six.vercel.app/",
         github:"https://github.com/Suraz95/the_book_vault.git"
     },
@@ -15,7 +18,7 @@ export const Work = [
         name:"Resume Builder",
         description:"A Resume Builder website build in ReactJS. With editing and downloading",
         tags:["React"],
-        
+        image:resume,
         demo:"https://suraz-resume-builder-react.vercel.app",
         github:"https://github.com/Suraz95/Suraz_resume_builder_react.git"
         
@@ -25,7 +28,7 @@ export const Work = [
         name:"Ecommerce Website",
         description:"An ecommerce website made using React nad Redux-tool-kit with add to cart and wishlist functionality",
         tags:["React","Redux-tool-kit"],
-        
+        image:ecommerce,
         demo:"https://suraz-ecommmerce-reduxtoolkit2.vercel.app/",
         github:"https://github.com/Suraz95/ecommmerce-reduxtoolkit2.git"
     },
@@ -34,7 +37,7 @@ export const Work = [
         name:"Expense Tracker",
         description:"An Expense Traker website which uses your local storage as your database and keeps track of your expenses ",
         tags:["react","styledComponents","react-router"],
-        
+        image:expense,
         demo:"https://suraz-expense-tracker.vercel.app/",
         github:"https://github.com/Suraz95/expense-tracker.git"
        
